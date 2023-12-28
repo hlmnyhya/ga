120d3a21350831b411763546ea42bae20744e9b1
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-12-28 08:47:51 +0800

    update data non asset

--- application/views/editdatanon_asset.php
+++ application/views/editdatanon_asset.php
@@ -31,7 +31,14 @@
             <div class="row mb-3">
                 <label for="satuan" class="col-sm-2 col-form-label">Satuan</label>
                 <div class="col-sm-7">
-                    <input type="text" class="form-control" name="satuan_non" required="" value="<?php echo $barang['satuan_non']; ?>">
+                    <select class="form-control" name="satuan_non">
+                        <option value="BUAH">BUAH</option>
+                        <option value="BUKU">BUKU</option>
+                        <option value="BOTOL">BOTOL</option>
+                        <option value="TABUNG">TABUNG</option>
+                        <option value="KOTAK">KOTAK</option>
+                        <option value="UNIT">UNIT</option>
+                    </select>
                 </div>
             </div>
             <div class="row mb-3">
